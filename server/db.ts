f79f8aae35ded2066cd444f437d3bdf46148c9e6
Emma Wang <emmawang@emmas-air.mynetworksettings.com>
2026-09-23 21:01:26 -0400

    done

--- server/db.ts
+++ server/db.ts
@@ -25,13 +25,18 @@ const pastQuestions: Question[] = sortQuestions([
         points: 400,
         question: 'When was the first iPhone released?',
         answer: '2007',
+    },
+        {
+        points: 500,
+        question: 'What black and white mammal has an extra thumb?';
+        answer: 'Panda',
     }
 ]);
 
 const presentQuestions: Question[] =
     sortQuestions([
         {
-            points: 400,
+            points: 500,
             question:
                 'What dog breed is this?',
             imgSrc: 'IMG_6324',
@@ -45,14 +50,21 @@ const presentQuestions: Question[] =
         },
         {
             points: 200,
+            question: 'What neighbourhood is Horace Mann in?',
+            answer: 'Riverdale',
+
+        },
+        {
+            points: 300,
             question: 'Who is the main character in To All the Boys I\'ve Loved Before',
             answer: 'Lara Jean',
         },
         {
-            points: 300,
+            points: 400,
             question:
                 'What art medium was created in 1934?',
             answer: 'Acrylic paint',
+
         }
     ]);
 const futureQuestions: Question[] = sortQuestions([
@@ -63,6 +75,13 @@ const futureQuestions: Question[] = sortQuestions([
         imgSrc:
             "statue-of-liberty-GettyImages-539667859",
         answer: 'New York City',
+    },
+{
+        points: 200,
+
+        question:
+            'Which continent has the top two most populated countries?',
+        answer: 'Asia',
     }
 ]);
 
